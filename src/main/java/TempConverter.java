97c68b109f013aa6796655c246952342f69d85ed
CooperStannard <03cooper.stannard@kamsc.org>
2025-10-03 13:13:47 -0400

    gh

--- src/main/java/TempConverter.java
+++ src/main/java/TempConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TempConverter {
     public static void main(String[] args) {
         // TODO: Implement temperature converter
@@ -7,5 +9,38 @@ public class TempConverter {
         // - Use formulas: C = (F - 32) * 5/9 and F = C * 9/5 + 32
         // - Display conversion results
         // - Handle invalid menu choices
+        Scanner sc = new Scanner(System.in);
+        Double temp;
+        boolean run;
+
+        run = true;
+        while (run){
+            System.out.println("----menu----");
+            System.out.println("1. F to C");
+            System.out.println("2. C to F");
+            System.out.println("3. exit");
+            System.out.print("Enter your choice:  ");
+            int choice = sc.nextInt();
+
+            switch (choice)
+            {
+                case 1:
+                    System.out.print("\nEnter the Temp in fahrenheit:  ");
+                    temp = sc.nextDouble();
+                    temp = (temp-32)*((double)5/9);
+                    System.out.println("\nThe celsius is "+ temp);
+                    break;
+                case 2:
+                    System.out.print("\nEnter the Temp in celsius:  ");
+                    temp = sc.nextDouble();
+                    temp = (temp*((double)9/5)+32);
+                    System.out.println("\nThe fahrenheit is "+ temp);
+                    break;
+                case 3:
+                    System.out.println("bye");
+                    run = false;
+                    break;
+            }
+        }
     }
 }
--- src/main/java/TimeConverter.java
+++ src/main/java/TimeConverter.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class TimeConverter {
     public static void main(String[] args) {
         // TODO: Implement time converter
@@ -6,5 +7,34 @@ public class TimeConverter {
         // - Display conversion results and calculation breakdown
         // - Ask user if they want to convert another time (y/n)
         // - Loop until user chooses to exit
+        int hour, second, minute, time;
+        boolean run = true;
+        Scanner sc = new Scanner(System.in);
+        while (run = true){
+        System.out.println("convert time to total seconds\n");
+        System.out.print("\nEnter hours: ");
+        hour = sc.nextInt();
+        System.out.print("Enter minutes: ");
+        minute = sc.nextInt();
+        System.out.print("Enter seconds: ");
+        second = sc.nextInt();
+
+        time = (hour * 3600)+ (minute * 60) + second;
+        System.out.println("\nTotal: " + time);
+
+        System.out.println("Caculation:");
+        System.out.println(hour +" hours * 3600 = "+(hour*3600)+ " seconds");
+        System.out.println(minute + " minutes * 60 = "+ (minute*60)+ " seconds");
+        System.out.println(second + " seconds = "+ second +" seconds");
+        System.out.println("\nTotal: " + time);
+
+        System.out.println("do you want to run the program again(y/n): ");
+        char runAgain = sc.next();
+        if (runAgain != "y")
+        {
+        run = false;
+        }
+
+        }
     }
 }
